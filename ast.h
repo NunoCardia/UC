192a03e0a09372d2e2a2ba30f5598d5b7d457fe3
Nuno Ferreira <nunocardia_21@hotmail.com>
2018-04-22 16:35:00 +0100

    Modified some comments

--- ast.h
+++ ast.h
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
 #include "symbol_table.h"
 
 #define MAX_STR 100
--- symbol_table.h
+++ symbol_table.h
@@ -18,3 +18,17 @@ typedef struct sym_table{
 
 
 sym_table *st,*temp;
+
+sym_table *create_node(char *node_type,char *name,char *type);
+tree_node *return_tree_node(tree_node *node, int son);
+int n_childs(tree_node* node);
+char *tolowercase(char *name);
+sym_table *create_variable_node(tree_node *node);
+sym_table *create_declaration_node(tree_node *node);
+sym_table *create_func_table_node(tree_node *node);
+void print_function(sym_table *node);
+int add_definition(sym_table *st, sym_table *table_node, tree_node *cur_node, sym_table *declaration_node);
+int add_to_top(sym_table *st, sym_table *node);
+void init_sym_table();
+void print_sym_table_elem(sym_table *element);
+void print_sym_table(sym_table *table);
